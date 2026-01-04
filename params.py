3b15dc591815f55f0c82b9d443b5696790ed3af4
Aiden Zhang <aidentzhang@gmail.com>
2026-01-03 23:25:57 -0700

    3 models loaded now. Current one in use had lowest loss.

--- params.py
+++ params.py
@@ -1,15 +1,15 @@
 #PARAM DECLARATIONS
 k_DModel = 384
-k_ContextLength = 96
+k_ContextLength = 32
 k_VocabSize = 4985+5 #plus five for start, end, pad, and space at start      last one is no idea, must manually set
 k_Attheads = 4
-k_AttBlocks = 4
+k_AttBlocks = 2
 k_DQuery = 96
 num_times = 5000
 
 k_ShiftFactor = 2
 k_BatchSize = 16
-k_Alpha = 0.0005
+k_Alpha = 0.0002
 k_Beta1 = 0.9
 k_Beta2 = 0.999
 k_Epsilon = 0.00000001
