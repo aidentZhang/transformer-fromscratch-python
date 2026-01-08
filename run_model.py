55d75cb6856f7864d3c1f27f02714f738400d4a9
Aiden Zhang <aidentzhang@gmail.com>
2026-01-08 11:58:40 -0500

    lightweight for aws lambda deployment

--- run_model.py
+++ run_model.py
@@ -1,9 +1,8 @@
-import mlx.core as mx
-import params
+import numpy as np
 
-weights = mx.load("./Weights/weights.npz")
+weights = np.load("./Weights/weights.npz")
 
-
+num_times = 5000
 
 sWe = weights["sWe"]
 sWpos = weights["sWpos"]
@@ -37,20 +36,20 @@ print(k_Dquery)
 
 
 def layerNorm(E, attLayer, prePostMLP):
-    temp = mx.nan_to_num((E-mx.mean(E, axis = -1, keepdims = True))/mx.sqrt((mx.nan_to_num(mx.var(E, axis = -1, keepdims = True), nan = 0.)+0.00001)))
+    temp = np.nan_to_num((E-np.mean(E, axis = -1, keepdims = True))/np.sqrt((np.nan_to_num(np.var(E, axis = -1, keepdims = True), nan = 0.)+0.00001)))
     return sLNBias[attLayer, prePostMLP] + temp * (sLNGain[attLayer, prePostMLP]), temp
 
 def softmax(E):
-    return mx.nan_to_num(mx.exp(E)/(mx.exp(E)@mx.ones((E.shape[1],1))), nan = 0)
+    return np.nan_to_num(np.exp(E)/(np.exp(E)@np.ones((E.shape[1],1))), nan = 0)
 
 def relu(E):
-    return mx.maximum(0, E)
+    return np.maximum(0, E)
 
 def relu_deriv(E):
-    return mx.minimum(1, E)
+    return np.minimum(1, E)
 
 def decode(E, svocabList):
-    temp = mx.argmax(E, axis = -1)
+    temp = np.argmax(E, axis = -1)
     answer = []
     for i in temp:
         i = int(i)
@@ -73,18 +72,18 @@ def decode(E, svocabList):
 def findLoss(E, input_llm, svocabDict):
     loss = 0
     i = 0
-    onehot_cache = mx.zeros((k_ContextLength, k_VocabSize))
+    onehot_cache = np.zeros((k_ContextLength, k_VocabSize))
 
     while i < len(input_llm):
         if input_llm[i] in svocabDict:
             onehot_cache[i, svocabDict[input_llm[i]]] = 1
-            loss += -mx.log(E[i][svocabDict[input_llm[i]]]+0.00001)
+            loss += -np.log(E[i][svocabDict[input_llm[i]]]+0.00001)
         else:
             onehot_cache[i, k_VocabSize-1] = 1
-            loss += -mx.log(E[i][k_VocabSize-1]+0.00001)
+            loss += -np.log(E[i][k_VocabSize-1]+0.00001)
 
         i+=1
-    loss = (loss-mx.log(E[len(input_llm)][1]+0.001))/len(input_llm)
+    loss = (loss-np.log(E[len(input_llm)][1]+0.001))/len(input_llm)
     onehot_cache[len(input_llm), 1] = 1
     return loss, onehot_cache
 
@@ -111,21 +110,20 @@ def embed(rule_list, case):
 def fowardprop(input_llm, svocabDict):
     #------------------
     #CACHEING
-    E_preln_cache = mx.zeros((k_AttBlocks, 2, k_ContextLength, k_DModel))
-    E_midln_cache = mx.zeros((k_AttBlocks, 2, k_ContextLength, k_DModel))
-    E_postln_cache = mx.zeros((k_AttBlocks, 2, k_ContextLength, k_DModel))
-    E_soft_cache = mx.zeros((k_AttBlocks, k_Attheads, k_ContextLength, k_ContextLength))
-    E_lin_cache = mx.zeros((k_ContextLength, k_DModel))
-    E_relu_cache = mx.zeros((k_AttBlocks, k_ContextLength, k_DModel*4))
+    E_preln_cache = np.zeros((k_AttBlocks, 2, k_ContextLength, k_DModel))
+    E_midln_cache = np.zeros((k_AttBlocks, 2, k_ContextLength, k_DModel))
+    E_postln_cache = np.zeros((k_AttBlocks, 2, k_ContextLength, k_DModel))
+    E_soft_cache = np.zeros((k_AttBlocks, k_Attheads, k_ContextLength, k_ContextLength))
+    E_lin_cache = np.zeros((k_ContextLength, k_DModel))
+    E_relu_cache = np.zeros((k_AttBlocks, k_ContextLength, k_DModel*4))
     
-    sSoftmaxMask =  mx.nan_to_num(-mx.inf*mx.triu(mx.ones((k_ContextLength, k_ContextLength)), k=1), nan = 0)
+    sSoftmaxMask = np.where(np.triu(np.ones((k_ContextLength, k_ContextLength)), k=1),-np.inf,0.0)
+    padMask = np.zeros((k_ContextLength, k_ContextLength))
+    padMask[:, len(input_llm)+1:k_ContextLength] = -np.inf
 
-    padMask = mx.zeros((k_ContextLength, k_ContextLength))
-    padMask[:, len(input_llm)+1:k_ContextLength] = -mx.inf
-
-    temp = mx.zeros(k_VocabSize)
+    temp = np.zeros(k_VocabSize)
     temp[2] = 1
-    We_to_E = mx.zeros((k_ContextLength, k_VocabSize))
+    We_to_E = np.zeros((k_ContextLength, k_VocabSize))
     We_to_E[0, 0] = 1
     i = 1
     for j in input_llm:
@@ -146,28 +144,25 @@ def fowardprop(input_llm, svocabDict):
 
     currAttBlock = 0
     while(currAttBlock < k_AttBlocks):
-        E_preln_cache[currAttBlock, 0] = mx.array(E)
+        E_preln_cache[currAttBlock, 0] = np.array(E)
         E_ln, E_midln_cache[currAttBlock, 0] = layerNorm(E, currAttBlock, 0)
-        E_postln_cache[currAttBlock, 0] = mx.array(E_ln)
+        E_postln_cache[currAttBlock, 0] = np.array(E_ln)
         currAttHead = 0
         while(currAttHead<k_Attheads):
-            E_soft_cache[currAttBlock, currAttHead] = softmax(1/mx.sqrt(k_DKey) * E_ln@sWq[currAttBlock, currAttHead]@(E_ln@sWk[currAttBlock, currAttHead]).T+sSoftmaxMask+padMask)
+            E_soft_cache[currAttBlock, currAttHead] = softmax(1/np.sqrt(k_DKey) * E_ln@sWq[currAttBlock, currAttHead]@(E_ln@sWk[currAttBlock, currAttHead]).T+sSoftmaxMask+padMask)
             E+= E_soft_cache[currAttBlock, currAttHead]@(E_ln@sWv[currAttBlock, currAttHead])
             currAttHead+=1
-        E_preln_cache[currAttBlock, 1] = mx.array(E)
+        E_preln_cache[currAttBlock, 1] = np.array(E)
         E_ln, E_midln_cache[currAttBlock, 1] = layerNorm(E, currAttBlock, 1)
-        E_postln_cache[currAttBlock, 1] = mx.array(E_ln)
+        E_postln_cache[currAttBlock, 1] = np.array(E_ln)
         E_relu_cache[currAttBlock] = relu(E_ln@sMLPW1[currAttBlock]+sMLPb1[currAttBlock])
         E += E_relu_cache[currAttBlock]@sMLPW2[currAttBlock]+sMLPb2[currAttBlock]
         currAttBlock+=1
-    E_lin_cache = mx.array(E)
+    E_lin_cache = np.array(E)
     E=E@sLW+sLB
     E=softmax(E)
 
     return E, E_midln_cache, E_soft_cache, E_lin_cache, E_relu_cache, E_postln_cache, E_preln_cache, We_to_E
-
-
-num_times = params.num_times
 
 with open('bpe_rules.txt', 'r') as f:
     rule_list = []
@@ -208,7 +203,7 @@ while(True):
     while k < k_ContextLength:
         E, E_midln_cache, E_soft_cache, E_lin_cache, E_relu_cache, E_postln_cache, E_preln_cache, We_to_E = fowardprop(q, svocabDict)
         prediction = decode(E, vocab_list)
-        loss, onehot_cache = findLoss(E, q, svocabDict)
+        # loss, onehot_cache = findLoss(E, q, svocabDict)
         # print(loss)
         # print(prediction)
         q.append(prediction[k])
